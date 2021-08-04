2da0dfec7fb07234a5c010fd7889f609547f6a99
KouNakagawa <rarai0626@gmail.com>
2021-08-04 11:28:25 +0000

    [Add]show_item_screen

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'c324c14300d93bec00353b313ed6eb46b4be504e32d3feb895a4c7bb2d212512feba899e8aa883e565333b05c4edd906849f5672672160977abd7b2a897bc85a'
