d26ca7381945d4b6d2f857fd9ed420e1742a5c0c
KouNakagawa <rarai0626@gmail.com>
2021-08-04 09:21:51 +0000

    [Add]item_registration_screen

---
+++ app/controllers/admins/items_controller.rb
@@ -0,0 +1,28 @@
+class Admins::ItemsController < ApplicationController
+  def index
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    item = Item.new(item_params)
+    item.save
+    redirect_to admin_item_path(item.id)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :image, :introduction, :price, :is_active)
+  end
+end
