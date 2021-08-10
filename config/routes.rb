7ee7bf8b1de44524f8362fceb7d3c654b5b3b723
KouNakagawa <rarai0626@gmail.com>
2021-08-10 07:51:30 +0000

    [Add] public_top_screen

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
   # ログイン時に使用されるviewファイルを指定
   devise_for :customers
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'public/homes#top'
+
   get 'admin' => 'admins/homes#top'
+
   get 'admin/genres' => 'admins/genres#index', as: 'admin/genres'
   post 'admin/genres' => 'admins/genres#create'
   get 'admin/genres/:id/edit' => 'admins/genres#edit', as: 'edit/admin/genres'
