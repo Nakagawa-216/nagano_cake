5e8accca9191a0062b2033495e5f3cef68f846ae
KouNakagawa <rarai0626@gmail.com>
2021-08-11 07:57:41 +0000

    [Add] public_registration_screen

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     admin_path
   end
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :first_name, :last_name_kana, :first_name_kana, :postal_code, :address, :telephone_number, :is_deleted])
+  end
 end
