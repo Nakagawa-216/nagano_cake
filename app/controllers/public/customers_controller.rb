c6ec64661fc46d9c28f8bb87d1c3eda84248eea1
KouNakagawa <rarai0626@gmail.com>
2021-08-12 10:06:35 +0000

    [Add] withdraw_screen

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -9,13 +9,17 @@ class Public::CustomersController < ApplicationController
   def update
     customer = Customer.find(current_customer.id)
     customer.update(customer_params)
-    redirect_to public_customer_path
+    redirect_to mypage_path
   end
 
   def unsubscribe
   end
 
   def withdraw
+    customer = Customer.find(current_customer.id)
+    customer.update(is_deleted: "true")
+    reset_session
+    redirect_to root_path
   end
 
   private
